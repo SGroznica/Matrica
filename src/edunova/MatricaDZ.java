e94f97dd1b44956bc736ab908e7aa1098cd666f7
SGroznica <93283616+SGroznica@users.noreply.github.com>
2021-12-26 20:35:22 +0100

    Dodan unos u matricu

--- src/edunova/MatricaDZ.java
+++ src/edunova/MatricaDZ.java
@@ -15,10 +15,16 @@ public class MatricaDZ {
 		int[][] matrica = new int[x][y];
 		for (int i = 0; i < x; i++) {
 				System.out.println(Arrays.toString(matrica[i]));
-
+				
 			}
-			System.out.println();
 		
+		for(int a=0;a<x;a++) {
+			for(int b=0;b<y;b++) {
+				matrica[a][b]=Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj"));
+			}
+			
+			System.out.println(Arrays.toString(matrica[a]));
+		}
 		}
 	}
 	
