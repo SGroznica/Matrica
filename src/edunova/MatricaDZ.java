d0cbb9816daf06b3ae7cef3a2cea3376e23832ad
SGroznica <93283616+SGroznica@users.noreply.github.com>
2021-12-28 21:26:18 +0100

    Dodan unos u redu i stupcu

--- src/edunova/MatricaDZ.java
+++ src/edunova/MatricaDZ.java
@@ -15,13 +15,18 @@ public class MatricaDZ {
 		int broj = 1;
 
 		int[][] matrica = new int[x][y];
-		for (int i = x-1; i >= 0; i--) {
-			// System.out.println(Arrays.toString(matrica[i]));
-			for (int j = y-1; j >= 0; j--) {
+		for (int i = x - 1; i >= x - 1; i--) {
+			for (int j = y - 1; j >= 0; j--) {
 				matrica[i][j] = broj++;
 
 			}
 
+		}
+
+		for (int i = x - 2; i >= 0; i--) {
+			for (int j = 0; j >= 0; j--) {
+				matrica[i][j] = broj++;
+			}
 		}
 
 		for (int a = 0; a < x; a++) {
